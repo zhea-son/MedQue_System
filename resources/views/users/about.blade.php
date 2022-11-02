20c8714eb8b0904447d1913523dc38d3eb6e6e3f
Suprav Kandel <dynosups@outlook.com>
2022-11-02 21:53:25 +0545

    Add authentication

--- resources/views/users/about.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('users.layouts.main')
-
-@section('content')
-    
-    <div class="min-h-screen bg-blue-100">
-        <p class="text-center">This is the About page</p>
-    </div>
-    
-@endsection
