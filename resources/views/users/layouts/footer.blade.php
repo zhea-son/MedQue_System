9960c22023bc903faec4bbd336f6a21aa950538d
SurajGayak <nepsuraj12@gmail.com>
2022-11-02 17:43:14 +0545

    footer coded

---
+++ resources/views/users/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+
+<div class="footer bg-bg-slate-200 flex  ">
+    <div class="logo">Queing System</div>
+    <i class="fa fa-facebook-official" aria-hidden="true"></i>Facebook
+    <i class="fa fa-instagram" aria-hidden="true"></i>Instagram
+    <i class="fa fa-twitter" aria-hidden="true"></i>Twitter
+</div>
