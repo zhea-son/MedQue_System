63ce4ecc697af35f921ffe12b5b16d6a33455173
Suprav Kandel <dynosups@outlook.com>
2022-11-02 22:21:45 +0545

    Fix layouts

---
+++ resources/views/users/layouts/header.blade.php
@@ -0,0 +1,16 @@
+<nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
+    <div class="mb-2 sm:mb-0">
+        <a href="/">Home</a>
+    </div>
+    <div>
+        <a href="/about">About</a>
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+    </div>
+</nav>
+
+
+
+
+
+
