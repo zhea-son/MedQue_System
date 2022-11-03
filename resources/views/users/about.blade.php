793b589dacd0049a95add14835c96f59d1834f09
SurajGayak <nepsuraj12@gmail.com>
2022-11-03 10:22:59 +0545

    about page added

---
+++ resources/views/users/about.blade.php
@@ -0,0 +1,36 @@
+<div class="herosection " style="background-color:rgb(0, 174, 255);margin:0; padding:0;">
+    <h1 style="font-weight:bold; text-align:center; font-size:70px;padding-top:10px;color:white;font-family:sans-serif;">About Us </h1>
+<h2 class="text-center" style="text-align:center; color:white;font-family:sans-serif;">
+    Make life easier with the Mediqueing System,by taking the service 
+</h2>
+<br>
+
+</div> 
+{{-- about us --}}
+<div class="about" style="width:60%;height:auto; margin:0 auto; font-family:sans-serif">
+    <h1 style="text-align:center;">Why us ???</h1>
+    <h3>Mediqueing system is the way of providing  facility to the patient online.Either it is appointment 
+    for the checkup or to get the report .It is more reliable and the faster because the patient doesnot need to stay 
+    on queue.    
+    </h3>
+    <button style="background-color:rgb(52, 93, 242); color:white;border:none;padding:10px;" >+</button>&nbsp&nbspFaster
+    <br><br>
+    <button style="background-color:rgb(52, 93, 242); color:white;border:none; padding:10px;">+</button>&nbsp&nbspReliable
+<br><br>
+<button style="background-color:rgb(52, 93, 242); color:white;border:none;padding:10px;">+</button>&nbsp&nbspEasier
+
+    </div>
+{{-- gallery --}}
+
+<div class="gallery" style="width:50%;height:auto; display:flex;justify-content:space-around;padding:30px;margin:0 auto;flex-wrap:wrap; gap:10px;">
+<img src="images/covid.jpg" style="width:200px;height:150px;">
+<img src="images/medicare.jpg" style="width:200px;height:150px;">
+<img src="images/covid.jpg" style="width:200px;height:150px; ">
+<img src="images/mediimg.jpg" style="width:200px;height:150px;">
+<img src="images/mediimg.jpg" style="width:200px;height:150px;">
+<img src="images/medicare.jpg" style="width:200px;height:150px;">
+
+</div>
+
+{{-- About Us --}}
+
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return view('users.about');
+});
+
 
 Route::get('/dashboard', function () {
     return view('users.dashboard');
