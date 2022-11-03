fd03d977725f1946c5887bcee949c73fd812d293
Ranjan <khanalranjan43@gmail.com>
2022-11-03 13:16:26 +0545

    Icon only

--- resources/views/users/layouts/header.blade.php
+++ resources/views/users/layouts/header.blade.php
@@ -1,11 +1,11 @@
 <nav class="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
     <div class="mb-2 sm:mb-0">
-        <a href="/" class="font-black" style="font-size:25px;">MediQueing System</a>
+        <a href="/" class="font-black" style="font-size:25px;">MediQueuing System</a>
     </div>
     <div class="px-5">
         <a href="/about">About</a>
         <a href="/login">Login</a>
-        <a href="/register">Register</a>
+        <a href="/">Register</a>
     </div>
 </nav>
 
