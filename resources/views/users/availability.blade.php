408e0ba3a33482ef1e2fdabe945a907e5d1877ae
SurajGayak <nepsuraj12@gmail.com>
2022-11-03 09:12:21 +0545

    availability added

--- resources/views/users/availability.blade.php
+++ resources/views/users/availability.blade.php
@@ -4,7 +4,70 @@
             {{ __('Dashboard') }}
         </h2> --}}
     </x-slot>
+    <div class="flex flex-col justify-center  text-center border-solid ">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8  ">
+          <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8  flex justify-center" >
+            <div class="overflow-hidden border-solid border-2 border-slate-600 my-5 "  style="padding:50px;">
+              <table class="min-w-100 ">
+                <thead class="bg-white ">
+                  <tr>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-8 py-4 text-left">
+                      #
+                    </th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-12 py-4 text-left">
+                     <b> Doctors</b>
+                    </th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-12 py-4 text-left">
+                      <b>Time</b>
+                    </th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-12 py-4 text-left">
+                      <b>Select</b>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      Mark
+                    </td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      10:00
+                    </td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                <button style="background-color:rgb(15, 96, 247);color:white;padding:5px;">Select</button>
 
-    <p>abcdefgh</p>
+                    </td>
+                  </tr>
+                  <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">2</td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      Jacob
+                    </td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      11:00
+                    </td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <button style="background-color:rgb(15, 96, 247);color:white;padding:5px;">Select</button>
+                    </td>
+                  </tr>
+                  <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">3</td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                      Larry
+                    </td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        12:00
+                    </td>
+                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <button style="background-color:rgb(15, 96, 247);color:white;padding:5px;">Select</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
 
 </x-app-layout>
